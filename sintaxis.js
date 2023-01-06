b29e05d97b3a22850ffb7b285a6f485f162387db
Ezequiel Toloza <ezequiel.toloza@gmail.com>
2023-01-06 20:21:23 -0300

    Modulo 14

---
+++ sintaxis.js
@@ -0,0 +1,18 @@
+
+//Comentario de una sola línea
+
+/**
+ * Esto es un
+ * comentario de
+ * muchas líneas
+ */
+
+var miVariable;
+let nombre;
+const IVA = 1.21;
+
+if (true) {
+    console.log("Hola");    
+} else {
+    console.log("Chau");   
+}
